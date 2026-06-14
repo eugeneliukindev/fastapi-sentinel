d0ec9910bc984ac9fb9286b0969d85c9071548a7
eugene <eugeneliukin.dev@gmail.com>
2026-06-14 15:57:02 +0300

    refactor: move UserOrm import to TYPE_CHECKING in RoleOrm

--- src/models/role.py
+++ src/models/role.py
@@ -5,10 +5,10 @@ from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import BaseOrm
-from src.models.user import UserOrm
 
 if TYPE_CHECKING:
     from .permission import PermissionOrm
+    from .user import UserOrm
 
 
 class RoleOrm(BaseOrm):
